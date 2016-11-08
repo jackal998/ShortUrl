49e5863315eff81ebec44882c31551c126ed421f
jackal998 <jackal998@gmail.com>
2016-11-08 17:17:05 +0800

    db:migrate

--- Short/app/controllers/ac_sus_controller.rb
+++ Short/app/controllers/ac_sus_controller.rb
@@ -1,2 +1,5 @@
 class AcSusController < ApplicationController
+	def index
+		
+	end
 end
--- Short/db/migrate/20161108090159_create_ac_sus.rb
+++ Short/db/migrate/20161108090159_create_ac_sus.rb
@@ -1,7 +1,9 @@
 class CreateAcSus < ActiveRecord::Migration[5.0]
   def change
     create_table :ac_sus do |t|
-
+    	t.string :target
+    	t.string :shorturl
+    	t.integer :used
       t.timestamps
     end
   end
